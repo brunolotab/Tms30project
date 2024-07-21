16ae89c5adae9f9f6b0ed01ce41c15b194b57e29
brunolotab <lotadiamond87@gmail.com>
2024-07-21 01:13:40 +0100

    fifth commit

--- Tm30project/src/Pages/Processors/ProcessorData/Processordata.jsx
+++ Tm30project/src/Pages/Processors/ProcessorData/Processordata.jsx
@@ -1,8 +1,7 @@
 
-
 export const Processordata =[
     {PID:'PROCESSOR 1D', PName: 'PROCESSOR NAME', PAdress:'PROCESSOR IP ADRESS', PPort:'PROCESSOR PORT NO.', PType:'PROCESSOR TYPE', PDesc: 'PROCESSOR DESCRIPTION'},
-    {ID:'POS1245', Name: 'Position', PAdress:'10.010.199.212', PPort:'5434.', PType:'POSITILLON NODE', PDesc: 'POSTILLON NODE 32113....'},
-    {ID:'NIB0057', Name: 'NIBBS', PAdress:'10.010.199.212', PPort:'2214', PType:'NIBBS NODE', PDesc: 'NIBBS NODE 32113.........'},
-    {ID:'TRANZ2392', Name: 'Tranzware(TIC)', PAdress:'10.010.199.212', PPort:'8371', PType:'TRANZWARE NODE', PDesc: 'TRANZWARE NODE 32113....'}
+    {ID:'POS1245', Name: 'Position', Adress:'10.010.199.212', Port:'5434.', Type:'POSITILLON NODE', Desc: 'POSTILLON NODE 32113....'},
+    {ID:'NIB0057', Name: 'NIBBS', Adress:'10.010.199.212', Port:'2214', Type:'NIBBS NODE', Desc: 'NIBBS NODE 32113.........'},
+    {ID:'TRANZ2392', Name: 'Tranzware(TIC)', Adress:'10.010.199.212', Port:'8371', Type:'TRANZWARE NODE', Desc: 'TRANZWARE NODE 32113....'}
 ]
--- Tm30project/src/Pages/Processors/Processors.jsx
+++ Tm30project/src/Pages/Processors/Processors.jsx
@@ -1,16 +1,66 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
+import { Processordata } from './ProcessorData/Processordata'
+import styled from 'styled-components';
 
 function Processors() {
+  const dataProcessor = Processordata;
   return (
-    <div>
-      <Navbar/>
-       <div className='flex justify-between align-middle bg-white p-3 border border-l-0 border-b-slate-200 border-t-0 px-6'  >
+    <Div>
+      <Navbar />
+      <div className='flex justify-between align-middle bg-white p-3 border border-l-0 border-b-slate-200 border-t-0 px-6'  >
         <div className='font-bold text-xl'>Processors</div>
         <div><button className='bg-color1 p-6 py-2 rounded-[10px] text-blue-700 font-bold text-[13px] '> + Add New Processors</button></div>
       </div>
-    </div>
+      <div className='text-[14px]'>
+        {
+          dataProcessor.map((item,id)=>{
+            return(
+              <div key={id}>
+                <table className='leading-8 w-[100%] text-[14px]' >
+                  <tr>
+                    <th>{item.PID}</th>
+                    <th>{item.PName}</th>
+                    <th>{item.PAdress}</th>
+                    <th>{item.PPort}</th>
+                    <th>{item.PType}</th>
+                    <th>{item.PDesc}</th>
+                  </tr>
+                  <tr>
+                    <td>{item.ID}</td>
+                    <td>{item.Name}</td>
+                    <td>{item.Adress}</td>
+                    <td>{item.Port}</td>
+                    <td>{item.Type}</td>
+                    <td>{item.Desc}</td>
+                  </tr>
+                </table>
+                </div>
+            )
+          })
+        }
+      </div>
+    </Div>
   )
 }
 
 export default Processors
+
+const Div = styled.div `
+  td {
+    text-align: left;
+    width: 5rem;
+    font-size: 13px;
+  }
+  tr:nth-child(odd) {
+    background-color: #fafafa;
+  }
+  th {
+    width: 7rem;
+    font-size: 14px;
+    border: 1px solid red;
+
+
+  }
+ 
+`
